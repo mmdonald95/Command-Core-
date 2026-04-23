b9c107bb90b7c8334c8d7b38bc1006459bf4af72
mmdonald95 <mmdonald95@gmail.com>
2026-04-23 17:05:01 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './src/lib/AuthContext.jsx'
-import { hasPermission, hasRole } from '@/lib/roles';
+import { AuthProvider } from './lib/AuthContext.jsx'
+import { hasPermission, hasRole } from './lib/roles.jsx'
 
 const Welcome = lazy(() => import('@/pages/Welcome'));
 const SignIn = lazy(() => import('@/pages/SignIn'));
