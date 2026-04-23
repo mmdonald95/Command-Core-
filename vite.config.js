7764f2a55d9cc0e6ef8e7ab157be313787fdc131
mmdonald95 <mmdonald95@gmail.com>
2026-04-23 16:20:35 -0500

    fix: remove unsupported vite plugins for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    },
-    dedupe: ['react', 'react-dom'],
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-    force: true,
-  },
-});
+})
